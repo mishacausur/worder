b31db36ec60cfdf3272c8ba4e97826c02d935021
Misha Causur <mishacausur@mail.ru>
2022-11-09 01:09:56 +0500

    word model (+ word type) has been added

---
+++ worder/Data/DataModels/WordModel.swift
@@ -0,0 +1,13 @@
+//
+//  WordModel.swift
+//  worder
+//
+//  Created by Misha Causur on 09.11.2022.
+//
+
+struct WordModel: Codable {
+    
+    let word: String
+    let translation: [String]
+    let type: WordType
+}
---
+++ worder/Data/DataModels/WordType.swift
@@ -0,0 +1,14 @@
+//
+//  WordType.swift
+//  worder
+//
+//  Created by Misha Causur on 09.11.2022.
+//
+
+enum WordType: Codable {
+    
+    case noun
+    case verb
+    case adjective
+    case idiom
+}
