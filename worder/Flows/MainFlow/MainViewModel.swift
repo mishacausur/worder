37ccb77732d2fa9a3c4a2a5ced8e476bb2727eb4
Misha Causur <mishacausur@mail.ru>
2022-11-17 20:55:57 +0500

    published subject has been added

--- worder/Flows/MainFlow/MainViewModel.swift
+++ worder/Flows/MainFlow/MainViewModel.swift
@@ -8,70 +8,14 @@
 import RxSwift
 
 final class MainViewModel: ViewModel {
+    private let reactive = ReactiveTestImpl()
     func getData() {
-        reactiveExampleMerge()
+        reactive.react()
         let manager = NetworkService()
         Task {
             await manager.getWords {
                 print($0)
             }
-        }
-    }
-    
-    private func reactiveExampleJust() {
-        example("just") {
-            let observable = Observable.just("Hi there, u'r using RxSwift now, congrats")
-            
-            observable.subscribe { print($0) }
-        }
-    }
-    
-    private func reactiveExampleOf() {
-        example("of") {
-            let observable = Observable.of(1, 2, 3, 4, 5)
-            observable.subscribe { print($0) }
-        }
-    }
-    
-    private func reactiveExampleCreate() {
-        example("create") {
-            let items = [1, 2, 3, 4, 5]
-            Observable.from(items).subscribe {
-                print($0)
-            } onError: {
-                print($0)
-            } onCompleted: {
-                print("completed")
-            } onDisposed: {
-                print("disposed")
-            }
-        }
-    }
-    
-    private func reactiveExampleFilter() {
-        example("filter") {
-            let sequence = Observable.of(1, 2, 4, 6, 10, 14, 27).filter { $0 >= 10 }
-            
-            sequence.subscribe { print($0) }
-        }
-    }
-    
-    private func reactiveExampleMap() {
-        example("map") {
-            let sequence = Observable.of(1, 2, 4, 6).map { "the number is \($0 * 10)" }
-            sequence.subscribe { print($0) }
-        }
-    }
-    
-    private func reactiveExampleMerge() {
-        example("merge") {
-            let firstThread = Observable.of("i", "t", "w")
-            let secondThread = Observable.of("m", "u", "o")
-            
-            let combine = Observable.of(firstThread, secondThread)
-            
-            let merger = combine.merge()
-            merger.subscribe { print($0) }
         }
     }
 }
---
+++ worder/ReactiveTest.swift
@@ -0,0 +1,85 @@
+//
+//  ReactiveTest.swift
+//  worder
+//
+//  Created by Misha Causur on 17.11.2022.
+//
+
+import RxSwift
+
+struct ReactiveTestImpl {
+    
+    func react() {
+        reactiveExamplePublished()
+    }
+    
+    // MARK: - FOUNDATION
+    
+    private func reactiveExampleJust() {
+        example("just") {
+            let observable = Observable.just("Hi there, u'r using RxSwift now, congrats")
+            
+            observable.subscribe { print($0) }
+        }
+    }
+    
+    private func reactiveExampleOf() {
+        example("of") {
+            let observable = Observable.of(1, 2, 3, 4, 5)
+            observable.subscribe { print($0) }
+        }
+    }
+    
+    private func reactiveExampleCreate() {
+        example("create") {
+            let items = [1, 2, 3, 4, 5]
+            Observable.from(items).subscribe {
+                print($0)
+            } onError: {
+                print($0)
+            } onCompleted: {
+                print("completed")
+            } onDisposed: {
+                print("disposed")
+            }
+        }
+    }
+    
+    private func reactiveExampleFilter() {
+        example("filter") {
+            let sequence = Observable.of(1, 2, 4, 6, 10, 14, 27).filter { $0 >= 10 }
+            
+            sequence.subscribe { print($0) }
+        }
+    }
+    
+    private func reactiveExampleMap() {
+        example("map") {
+            let sequence = Observable.of(1, 2, 4, 6).map { "the number is \($0 * 10)" }
+            sequence.subscribe { print($0) }
+        }
+    }
+    
+    private func reactiveExampleMerge() {
+        example("merge") {
+            let firstThread = Observable.of("i", "t", "w")
+            let secondThread = Observable.of("m", "u", "o")
+            
+            let combine = Observable.of(firstThread, secondThread)
+            
+            let merger = combine.merge()
+            merger.subscribe { print($0) }
+        }
+    }
+    
+    // MARK: - SUBJECTS
+    
+    private func reactiveExamplePublished() {
+        let disposableBag = DisposeBag()
+        let subject = PublishSubject<String>()
+        subject.subscribe { print($0) }.disposed(by: disposableBag)
+        subject.on(Event<String>.next("Hi there"))
+        subject.on(.next("it's working using RxSwift now"))
+        subject.onNext("It's amazing, isn't it?")
+    }
+}
