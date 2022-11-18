0be1f0c6f14a77ae9d2c7ae71c556f72e8d21b4e
Misha Causur <mishacausur@mail.ru>
2022-11-18 23:25:50 +0500

    rx to slider + progress added

--- worder/Flows/MainFlow/MainView.swift
+++ worder/Flows/MainFlow/MainView.swift
@@ -11,6 +11,15 @@ import UIKit
 final class MainView: Viеw {
     
     // MARK: - UI
+    private let slider = UISlider().configure {
+        $0.tintColor = .orange
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
+    private let progress = UIProgressView(progressViewStyle: .bar).configure {
+        $0.tintColor = .green
+        $0.progressTintColor = .orange
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
     private let label = UILabel().configure {
         $0.layer.borderColor = UIColor.darkGray.cgColor
         $0.layer.borderWidth = 1
@@ -35,6 +44,10 @@ final class MainView: Viеw {
         $0.font = .systemFont(ofSize: 24, weight: .bold)
         $0.translatesAutoresizingMaskIntoConstraints = false
     }
+    private let progressLable = UILabel().configure {
+        $0.textColor = .orange
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
     
     // MARK: - PROPERTIES
     private let disposeBag = DisposeBag()
@@ -43,7 +56,14 @@ final class MainView: Viеw {
     private var countText = 0
     
     override func addViews() {
-        addViews(label, textField, button, countButton, counter)
+        addViews(label,
+                 textField,
+                 button,
+                 countButton,
+                 counter,
+                 slider,
+                 progress,
+                 progressLable)
     }
     
     override func bindViews() {
@@ -80,18 +100,43 @@ final class MainView: Viеw {
             .asObservable()
             .subscribe(onNext: { print($0) })
             .disposed(by: disposeBag)
+/*
+        slider.rx.value
+            .asDriver()
+            .drive(progress.rx.progress)
+            .disposed(by: disposeBag)
+*/
+        slider.rx.value
+            .asDriver()
+            .drive(onNext: { [weak self] in
+                self?.progressLable.textColor = $0 > 0.5 ? .white : .orange
+                self?.progress.progress = $0
+                self?.progressLable.text = "\(Int($0 * 100))%"
+            })
+            .disposed(by: disposeBag)
     }
     
     override func layout() {
         backgroundColor = .white
-        [label, textField, button, countButton].forEach {
+        [label, textField, button, countButton]
+            .forEach {
             $0.layer.cornerRadius = 8
             $0.layer.masksToBounds = true
         }
-        [textField.centerXAnchor.constraint(equalTo: centerXAnchor),
+        
+        [slider.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 44),
+         slider.centerXAnchor.constraint(equalTo: centerXAnchor),
+         slider.widthAnchor.constraint(equalToConstant: 300),
+         progress.topAnchor.constraint(equalTo: counter.bottomAnchor, constant: 22),
+         progress.leadingAnchor.constraint(equalTo: leadingAnchor),
+         progress.trailingAnchor.constraint(equalTo: trailingAnchor),
+         progress.heightAnchor.constraint(equalToConstant: 44),
+         progressLable.centerXAnchor.constraint(equalTo: progress.centerXAnchor),
+         progressLable.centerYAnchor.constraint(equalTo: progress.centerYAnchor),
+         textField.centerXAnchor.constraint(equalTo: centerXAnchor),
          textField.centerYAnchor.constraint(equalTo: centerYAnchor),
          textField.heightAnchor.constraint(equalToConstant: 66),
-         textField.widthAnchor.constraint(equalToConstant: 144),
+         textField.widthAnchor.constraint(equalToConstant: 244),
          label.centerXAnchor.constraint(equalTo: centerXAnchor),
          label.bottomAnchor.constraint(equalTo: textField.topAnchor, constant: -44),
          label.heightAnchor.constraint(equalTo: textField.heightAnchor),
