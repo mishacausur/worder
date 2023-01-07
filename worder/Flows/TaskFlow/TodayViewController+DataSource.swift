165c878d18880196489fe01dc0c4d21b03dab6c0
Misha Causur <mishacausur@mail.ru>
2023-01-07 23:46:09 +0500

    cell configuration completed

--- worder/Flows/TaskFlow/TodayViewController+DataSource.swift
+++ worder/Flows/TaskFlow/TodayViewController+DataSource.swift
@@ -20,8 +20,22 @@ extension TodayView {
         defaultConfiguration.secondaryText = item.dueDate.dateAndTimeText
         defaultConfiguration.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
         cell.contentConfiguration = defaultConfiguration
+        var doneConf = doneItemConfiguration(for: item)
+        doneConf.tintColor = .todayListCellDoneButtonTint
+        cell.accessories = [.customView(configuration: doneConf), .disclosureIndicator(displayed: .always)]
         var backgroundConfig = UIBackgroundConfiguration.listGroupedCell()
         backgroundConfig.backgroundColor = .todayListCellBackground
         cell.backgroundConfiguration = backgroundConfig
+    }
+    
+    private func doneItemConfiguration(for reminder: Reminder) -> UICellAccessory.CustomViewConfiguration {
+        let iconName = reminder.isComplete ? "circle.fill" : "circle"
+        let symbolConfiguration = UIImage.SymbolConfiguration(textStyle: .title1)
+        let image = UIImage(systemName: iconName, withConfiguration: symbolConfiguration)
+        let button = UIButton()
+        button.setImage(image, for: .normal)
+        return .init(customView: button, placement: .leading(displayed: .always))
+        
+        
     }
 }
