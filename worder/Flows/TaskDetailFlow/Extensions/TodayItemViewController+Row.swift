e70bbb03a98684760d0ba983c98bf10e43a52ff3
Misha Causur <mishacausur@mail.ru>
2023-01-08 20:53:33 +0500

    today item controller has been setup

---
+++ worder/Flows/TaskDetailFlow/Extensions/TodayItemViewController+Row.swift
@@ -0,0 +1,47 @@
+//
+//  TodayItemViewController+Row.swift
+//  worder
+//
+//  Created by Misha Causur on 08.01.2023.
+//
+
+import UIKit
+
+extension TodayItemViewController {
+    enum Row: Hashable {
+        case date
+        case note
+        case time
+        case title
+        
+        var imageName: String? {
+            switch self {
+            case .date:
+                return "calendar.circle"
+            case .note:
+                return "square.and.pencil"
+            case .time:
+                return "clock"
+            default:
+                return nil
+            }
+        }
+        
+        var image: UIImage? {
+            guard let imageName = imageName else {
+                return nil
+            }
+            let conf = UIImage.SymbolConfiguration(textStyle: .headline)
+            return .init(systemName: imageName, withConfiguration: conf)
+        }
+        
+        var textStyle: UIFont.TextStyle {
+            switch self {
+            case .title:
+                return .headline
+            default:
+                return .subheadline
+            }
+        }
+    }
+}
--- worder/Flows/TaskDetailFlow/TodayItemViewController.swift
+++ worder/Flows/TaskDetailFlow/TodayItemViewController.swift
@@ -5,11 +5,14 @@
 //  Created by Misha Causur on 08.01.2023.
 //
 
-import class UIKit.UICollectionViewController
 import UIKit.UICollectionLayoutList
+import UIKit.UIDiffableDataSource
+import class UIKit.UICollectionViewController
 
 final class TodayItemViewController: UICollectionViewController {
     
+    private typealias DataSource = UICollectionViewDiffableDataSource<Int, Row>
+    private var dataSource: DataSource!
     var reminder: Reminder
     
     init(reminder: Reminder) {
@@ -22,5 +25,34 @@ final class TodayItemViewController: UICollectionViewController {
     
     required init?(coder: NSCoder) {
         fatalError("Always initialize TodayItemViewController using init(reminder:)")
+    }
+    
+    override func viewDidLoad() {
+        let cellReg = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
+        dataSource = .init(collectionView: collectionView, cellProvider: {
+            return $0.dequeueConfiguredReusableCell(using: cellReg, for: $1, item: $2)
+        })
+    }
+    
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, row: Row) {
+        var conf = cell.defaultContentConfiguration()
+        conf.text = getText(for: row)
+        conf.textProperties.font = UIFont.preferredFont(forTextStyle: row.textStyle)
+        conf.image = row.image
+        cell.contentConfiguration = conf
+        cell.tintColor = .todayPrimaryTint
+    }
+    
+    func getText(for row: Row) -> String? {
+        switch row {
+        case .date:
+            return reminder.dueDate.dayText
+        case .note:
+            return reminder.notes
+        case .time:
+            return reminder.dueDate.formatted(date: .omitted, time: .shortened)
+        case .title:
+            return reminder.title
+        }
     }
 }
