33ae92f594aabdc054381618ec7519b0295f00f9
Misha Causur <mishacausur@mail.ru>
2023-01-14 01:30:33 +0500

    reminders states added

--- worder/Flows/TaskFlow/TodayViewModel.swift
+++ worder/Flows/TaskFlow/TodayViewModel.swift
@@ -5,7 +5,29 @@
 //  Created by Misha Causur on 04.01.2023.
 //
 
+import Foundation
+
 final class TodayViewModel: ViewModel {
+    
+    enum ReminderState: Int {
+        case today
+        case future
+        case all
+        
+        func shouldInclude(_ date: Date) -> Bool {
+            let isToday = Locale.current.calendar.isDateInToday(date)
+            
+            switch self {
+            case .today:
+                return isToday
+            case .future:
+                return (date > Date.now) && !isToday
+            case .all:
+                return true
+            }
+        }
+    }
+    
     func coordinateDetails(with item: Reminder, onChange: @escaping (Reminder) -> Void) {
         coordinator?.route(.todayDetail(item, onChange: onChange))
     }
